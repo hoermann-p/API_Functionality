5c93933d85093a96132d2b489d39d56ef7fe720b
hoermann-p <54279584+hoermann-p@users.noreply.github.com>
2021-12-13 20:02:21 +0100

    Edit Comments

--- en_decrypt.php
+++ en_decrypt.php
@@ -2,6 +2,7 @@
 
 
 // deoending if either "d" or "e" is passed through encryptDecrypt the function will decrypt/encrypt the values
+// This function uses a secret key ( usually IP of the requesting server ) and a secret iv ( usually api_key of requesting user )
 
 function en_decrypt($stringToHandle = "",$encryptDecrypt = 'e', $ip, $api_key){
 
@@ -9,8 +10,8 @@ global $glob_arr;
 // Set default output value
 $output = null;
 // Set secret keys
-$secret_key = $ip; // Change this!
-$secret_iv = $api_key; // Change this!
+$secret_key = $ip; 
+$secret_iv = $api_key; 
 $key = hash('sha256', $secret_key) ;
 $iv = substr(hash('sha256', $secret_iv ),0,16);
 // Check whether encryption or decryption
